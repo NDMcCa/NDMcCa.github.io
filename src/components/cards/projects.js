dd0170cc4f5405d71031af16f40824170629b84d
N_McCamis <nicholas.m19@gmail.com>
2024-05-24 12:08:23 -0600

    Setup for travel page, staging for updating experience/projects

--- src/components/cards/projects.js
+++ src/components/cards/projects.js
@@ -12,15 +12,15 @@ const projects = [
     "title": "'ndmcca.github.io' (this website)",
     "img": "./images/webDev.png",
     "description": "This website indeed is an actual project of mine, built from the ground up",
-    "tags": "JavaScript, HTML5, CSS, DOM, Node.JS, React",
+    "tags": "JavaScript, HTML5, CSS, DOM, Node.JS, React (NPM)",
     "link": "https://github.com/NDMcCa/NDMcCa.github.io",
     "key": 2
 },
 {
     "title": "Elementary Data Strutures",
     "img": "./images/rbt.png",
-    "description": "A project I worked on in a software engineering course utilizing a given hardware facade developed in Java",
-    "tags": "Java, Interfaces",
+    "description": "Examples of elementary data structures implemented in Java and also in .NET/C# demonstrating how data structures may be implemented and the difference between two very similar languages. Includes interfaces for each data structure.",
+    "tags": "Java, C#, Interfaces",
     "link": "https://github.com/NDMcCa/Data-structures",
     "key": 3
 },
@@ -33,13 +33,21 @@ const projects = [
     "key": 4
 },
 {
-    "title": "Stock Screener",
+    "title": "Basic Algorithmic Paradigms in Python",
     "img": "./images/comingSoon.png",
-    "description": "Still in early development. The goal is to build a React webapp that can be used to screen stocks based on user-defined criteria using data from price feed oracle APIs.",
-    "tags": "React, Node.js, API, Git",
+    "description": "A collection of basic algorithmic paradigms implemented in Python. Includes examples of greedy algorithms, divide and conquer, dynamic programming, and more. Also includes a basic example use of the pySAT (Satisfiability) library.",
+    "tags": "Python, Algorithms, Complexity, pySAT Library",
     "link": "#",
     "key": 5
-}
+},
+// {
+//     "title": "Stock Screener",
+//     "img": "./images/comingSoon.png",
+//     "description": "Still in early development. The goal is to build a React webapp that can be used to screen stocks based on user-defined criteria using data from price feed oracle APIs.",
+//     "tags": "React, Node.js, API, Git",
+//     "link": "#",
+//     "key": 6
+// }
 ]
 
 export default projects;
---
+++ src/pages/travel/Travel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Travel.css';
+import '../../App.css';
+import Navbar from '../../components/navbar//navbar.js';
+
+function Travel() {
+    return (
+        <>
+        <div className="App-header">
+            <Navbar />
+        </div>
+            <div className="App-header">
+        </div>
+        </>
+    )
+
+}
